0ab0cb3667e9b6cc2b91b7c2199b4d1874eb8c66
Emile <bvcxzq1@gmail.com>
2023-01-29 13:16:01 +0000

    Add form and styling for game area.

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+// Which fortified world of the Imperium watches over the Eye of Terror?, Jago, Cadia, Titan, Boros Prime
+
+/*
+This function is to check the answers. Run when button is clicked
+*/
+function checkAnswers () {
+    let radios = 
+}
