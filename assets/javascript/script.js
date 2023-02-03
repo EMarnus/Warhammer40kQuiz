21a204c3c1ed2e2c51b5b4ac4c77a05ed817f0ad
Emile <bvcxzq1@gmail.com>
2023-02-03 18:41:48 +0000

    Add script to increment and update score to DOM.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,6 +9,8 @@ console.log("JS Loaded");
 // let answer3 = document.getElementById("question3").innerHTML;
 
 let gameNumber; //This is the index number of current question.
+let gameScore; //For global tracking of game score
+console.log(gameScore)
 
 /*
 This  stores and retreives Username and score.
@@ -129,7 +131,6 @@ function startGame () {
 
 
 
-
 /*
 This function is to check the answers. Run when button is clicked
 */
@@ -151,10 +152,18 @@ function checkAnswer() {
       //console.log(selected);
 
       if (gameNumber == spaceMarineQuestions.length-1) {
-        console.log("End of quiz") //Trigger Model
+        //console.log("End of quiz") 
+        openModal (); //Trigger Model
       } else if (selected == spaceMarineQuestions[gameNumber].answer){
         //Something to indicate correct answers
-        nextQuestions ()
+        nextQuestions ();
+        if (!gameScore){
+          gameScore = 0;
+          console.log(gameScore)
+        }
+        gameScore = gameScore + 1;
+        document.getElementById("game-score").innerHTML = gameScore;
+        console.log(gameScore)
       } else {
         alert(`Incorrect, the correct answer is ${spaceMarineQuestions[gameNumber].answer}.`);
       }
@@ -173,4 +182,29 @@ function checkAnswer() {
 
   }
   
-  
+
+/*
+Scripts to control the Modal from https://www.w3schools.com/howto/howto_css_modals.asp
+*/
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
